2d3efdb23783d3c3ec41de404831b8a6a5e80e2b
Florian Breisch <florianbreisch@b-lebt.de>
2012-12-11 10:26:38 +0100

    refactored lotto

--- lotto.cpp
+++ lotto.cpp
@@ -5,7 +5,30 @@
 using namespace std;
 
 namespace Lotto {
-  class Draws {
-    
+  class Draw {
+  private:
+    int numbers[6];
+  
+  public:
+    Draw();
+    void drawNumbers(Random::Table &randomTable);
   };
+  
+  Draw::Draw() {
+    for (int i = 0; i < 6; i++) {
+      numbers[i] = 0;
+    }
+  }
+  
+  void Draw::drawNumbers(Random::Table &randomTable) {
+    int currentNumber;
+    
+    for (int i = 0; i < 6; i++) {
+      currentNumber = randomTable.random(1, 49 - i);
+      for (int j = 0; j <= i; j++) {
+        if (numbers[j] < currentNumber)
+          // kann schief gehen..
+      }
+    }
+  }
 }
